b33445a2297e48de6e9584158f6be48e07a7bfda
Andrii Tkachenko <andrii.tkachenko@ukma.edu.ua>
2021-10-04 22:12:25 +0300

    Fixed starter

---
+++ db-jpa-boot-starter/src/main/java/ukma/db/dbjpabootstarter/DbJpaBootStarterApplication.java
@@ -0,0 +1,13 @@
+package ukma.db.dbjpabootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJpaBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbJpaBootStarterApplication.class, args);
+    }
+
+}
---
+++ db-jpa-boot-starter/src/test/java/ukma/db/dbjpabootstarter/DbJpaBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ukma.db.dbjpabootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbJpaBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
