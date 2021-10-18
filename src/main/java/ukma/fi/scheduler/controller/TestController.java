28fb555bc76e1f7661821c1971f076ac91ac352e
Andrii Tkachenko <andrii.tkachenko@ukma.edu.ua>
2021-10-18 17:32:02 +0300

    Created logback.xml

--- src/main/java/ukma/fi/scheduler/controller/TestController.java
+++ src/main/java/ukma/fi/scheduler/controller/TestController.java
@@ -1,5 +1,6 @@
 package ukma.fi.scheduler.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,20 @@ import ukma.fi.scheduler.repository.UserRepository;
 import ukma.fi.scheduler.service.AuthService;
 
 @RestController
+@Log4j2
 public class TestController {
+
 
     @Autowired
     private AuthService authService;
 
     @GetMapping("/lol")
     public User login(){
+        log.trace("some trace logging...");
+        log.debug("some debug logging...");
+        log.info("some info logging...");
+        log.warn("some warn logging...");
+        log.error("some error logging...");
         return authService.getUserInfo(1l);
     }
 
