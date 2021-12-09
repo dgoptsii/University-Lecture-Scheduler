921b3ab7aa4802dcf677b36449c22208e05f8802
Andrii Tkachenko <andrii.tkachenko@ukma.edu.ua>
2021-12-09 14:41:30 +0200

    ServiceTest Example

---
+++ src/test/java/ukma/fi/scheduler/controller/UserServiceTest.java
@@ -0,0 +1,47 @@
+package ukma.fi.scheduler.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ukma.fi.scheduler.entities.User;
+import ukma.fi.scheduler.repository.UserRepository;
+import ukma.fi.scheduler.service.UserService;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    public void mockService() {
+
+        User u = new User();
+        u.setLogin("login1");
+        u.setPassword("pass");
+        u.setStatus("STUDENT");
+
+        doReturn(Optional.of(u)).when(userRepository).findByLogin("login1");
+        doReturn(new ArrayList<User>()).when(userRepository).findAllByStatus("login1");
+        doReturn(u).when(userRepository).save(any(User.class));
+    }
+
+
+    @Test
+    public void test() {
+        Assertions.assertEquals("login1", userService.findUserByLogin("login1").getLogin());
+    }
+
+}
